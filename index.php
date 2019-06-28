1f5ba91e107a22c82b374c2427882b41ea4ff96a
ajaykpv <ajayputhanveettil@gmail.com>
2019-06-28 12:07:00 +0530

    add existing file

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+// set page header
+$page_title = "Read Products";
+include_once "layout_header.php";
+
+// contents will be here
+
+// set page footer
+include_once "layout_footer.php";
+?>
